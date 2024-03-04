73b4294b00f5683bb890b6f6e3b3e60965d2fc4e
Deepak <Deepak@TYSS-Deepak>
2024-03-04 16:05:02 +0530

    test

---
+++ HRMApplication/src/main/resources/SampleTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void sampleTest() {
+		System.out.println("execute test1");
+	}
+	
+	@Test
+	public void sampleTest() {
+		System.out.println("execute test1");
+	}
+
+}
