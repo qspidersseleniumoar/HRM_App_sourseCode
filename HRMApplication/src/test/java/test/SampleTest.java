e73a7ea647586e524a02151bdb8cd7441ad0c276
Deepak <Deepak@TYSS-Deepak>
2024-03-04 16:35:08 +0530

    Push to master

--- HRMApplication/src/test/java/test/SampleTest.java
+++ HRMApplication/src/test/java/test/SampleTest.java
@@ -1,15 +1,22 @@
+package test;
+
 import org.testng.annotations.Test;
 
 public class SampleTest {
 	
 	@Test
-	public void sampleTest() {
+	public void sampleTest1() {
 		System.out.println("execute test1");
 	}
 	
 	@Test
-	public void sampleTest() {
+	public void sampleTest2() {
 		System.out.println("execute test1");
+	}
+
+	@Test
+	public void sampleTest3() {
+		System.out.println("execute test3");
 	}
 
 }
