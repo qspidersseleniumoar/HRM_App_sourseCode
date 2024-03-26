076c6348f3c02f3fc393ab6313b09267dd6aa249
Deepak <Deepak@TYSS-Deepak>
2024-03-26 17:03:52 +0530

    final commit

--- HRMApplication/src/test/java/test/SampleTest.java
+++ HRMApplication/src/test/java/test/SampleTest.java
@@ -5,63 +5,12 @@ import org.testng.annotations.Test;
 public class SampleTest {
 	
 	@Test
-	public void sampleTest1() {
+	public void sampleTest() {
 		System.out.println("execute test1");
+		
+		String data = System.getProperty("URL", "defaultData");
+		System.out.println(data);
 	}
 	
-	@Test
-	public void sampleTest2() {
-		System.out.println("execute test1");
-	}
-
-	@Test
-	public void sampleTest3() {
-		System.out.println("execute test3");
-	}
-	@Test
-	public void sampleTest4() {
-		System.out.println("execute test4");
-	}
-	
-	@Test
-	public void sampleTest11() {
-		System.out.println("execute test11");
-	}
-	
-	@Test
-	public void sampleTest12() {
-		System.out.println("execute test12");
-	}
-	
-	
-	@Test
-	public void sampleTest5() {
-		System.out.println("execute test5");
-	}
-	
-	@Test
-	public void sampleTest6() {
-		System.out.println("execute test6");
-	}
-	
-	@Test
-	public void sampleTest7() {
-		System.out.println("execute test7");
-	}
-	
-	@Test
-	public void sampleTest9() {
-		System.out.println("execute test9");
-	}
-	
-	@Test
-	public void sampleTest10() {
-		System.out.println("execute test10");
-	}
-	
-	@Test
-	public void sampleTest8() {
-		System.out.println("execute test8");
-	}
 
 }
